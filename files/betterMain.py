bd44d0aa68905d5216f98aaee3a98e3abb47b18e
Adam N <121262498+FloppaDev4000@users.noreply.github.com>
2023-09-21 09:29:48 +0100

    uh huh

--- files/betterMain.py
+++ files/betterMain.py
@@ -5,23 +5,33 @@ fileName = "days.csv"
 
 entryInput = input("Entry? >> ")
 
-today = str(datetime.date.today())
+today = datetime.date.today()
+now = datetime.datetime.now()
 print(today) #edit out later
 
 # reading file
 with open(fileName, "r", encoding="utf-8") as fileObjR:
     r = csv.reader(fileObjR)
-    doneToday = today == r[-1][0]
+    entryList = []
+    for row in r:
+        entryList.append(row)
+    doneToday = str(today) == entryList[-1][0]
+    fileObjR.close()
 
 # here compare today w/ most recent entry
 if doneToday:
     # replace recent entry w/ new thing
     print("Replaced.")
 else:
-    # append list w/ new entry
+    with open(fileName, "w", encoding="utf-8") as fileObjW:
+        w = csv.writer(fileObjW)
+        
+        fullEntry = [str(today),str(now.strftime("%H:%M:%S")),entryInput]
+        w.writeRow(fullEntry)
+        
+        fileObjW.close()
     print("Appended.")
 
-viewInput = input("View? >>")
+viewInput = input("View? >> ")
 if viewInput == "Y":
-    # print recent entries (or all)
-    pass #edit out later
+    print(entryList)
