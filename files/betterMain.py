371bfa6186593a5b39356d67641b33c6af06e8bf
Adam N <121262498+FloppaDev4000@users.noreply.github.com>
2023-09-21 18:42:37 +0100

    i forgor

--- files/betterMain.py
+++ files/betterMain.py
@@ -8,8 +8,8 @@ fileName = "files/days.csv"
 
 dFont = "Calibri"
 dPadx = 20
-dPady = 15
-entryInput = "yes" #input("Entry? >> ")
+dPady = 12
+# entryInput = "yes" #input("Entry? >> ")
 
 
 today = date.today().strftime("%d-%m-%Y")
@@ -26,9 +26,12 @@ class mainWindow:
         
         # text entry
         self.entryBox = tk.Text(self.root)
+
+        # warning label
+        self.warning = tk.Label(self.root, text="", font=(dFont, 10))
         
         # submit button
-        self.entryButton = tk.Button(self.root, text="Submit Entry")
+        self.entryButton = tk.Button(self.root, text="Submit Entry", command=lambda: self.writeEntry(entryList))
         
         # view past entries button
         self.viewButton = tk.Button(self.root, text="View Past Entries", command=self.viewEntries)
@@ -36,10 +39,12 @@ class mainWindow:
         # pack everything
         self.mainLabel.pack(padx=dPadx, pady=dPady)
         self.entryBox.pack(padx=dPadx, pady=dPady)
+        self.warning.pack(padx=dPadx, pady=5)
         self.entryButton.pack(padx=dPadx, pady=dPady)
         self.viewButton.pack(padx=dPadx, pady=dPady)
         self.root.mainloop()
     
+    # second text box
     def viewEntries(self):
         entriesWindow = tk.Toplevel(self.root)
 
@@ -47,24 +52,32 @@ class mainWindow:
 
         entriesWindow.geometry("500x350")
 
-        self.words = tk.Label(entriesWindow,
-                              text=fullData,
-                              anchor="w",
-                              justify="left",
+        self.words = tk.Text(entriesWindow,
                               width=50)
+        
         self.words.pack(padx=dPadx, pady=dPady)
-
-
-
-
-def writeEntry(writeType):
-    with open(fileName, writeType, encoding="utf-8", newline="") as fileObjW:
-        w = csv.writer(fileObjW)
+        self.words.insert("1.0", str(formattedEntries))
+        self.words.config(state="disabled")
+    
+    # write to the CSV file
+    def writeEntry(self, list_of_entries):
         
-        fullEntry = [str(today),str(now.strftime("%H:%M:%S")),entryInput]
-        w.writerow(fullEntry)
-        
-        fileObjW.close()
+        entryInput = self.entryBox.get("1.0", tk.END)
+
+        if entryInput != "\n":
+            with open(fileName, "w", encoding="utf-8", newline="") as fileObjW:
+                w = csv.writer(fileObjW)
+                
+                fullEntry = [str(today),str(now.strftime("%H:%M:%S")),entryInput.replace("\n", "")]
+                list_of_entries.append(fullEntry)
+                for i in list_of_entries:
+                    w.writerow(i)
+                
+                fileObjW.close()
+        else:
+            self.warning.config(text = "There's nothing in the entry box!")
+            print("didnt work")
+
 
 # reading file
 with open(fileName, "r", encoding="utf-8") as fileObjR:
@@ -79,13 +92,19 @@ with open(fileName, "r", encoding="utf-8") as fileObjR:
         doneToday = str(today) == entryList[-1][0]
     fileObjR.close()
 
+formattedEntries = ""
+for i in entryList:
+    formattedEntries = formattedEntries + str(i[0]) + ", " + str(i[1]) + " >> " + str(i[2]) + "\n\n"
+
 # here compare today w/ most recent entry
+'''
 if doneToday:
-    writeEntry("w")
+    writeEntry(entryList)
     print("Replaced.")
 else:
     writeEntry("a")
     print("Appended.")
+
 
 viewInput = "Y" #input("View? >> ")
 if viewInput == "Y":
@@ -96,5 +115,5 @@ if viewInput == "Y":
             entry = row2[0] + ",", row2[1] + " : \"" + row2[2] + "\""
             fullData.append(entry)
     print()
-
+'''
 mainWindow()
